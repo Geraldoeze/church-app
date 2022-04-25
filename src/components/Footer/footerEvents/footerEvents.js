ae81cdab391f5ff2397c7a21060a8540fc88157c
Gerald Ezenagu <egerald344@gmail.com>
2022-04-25 18:44:38 +0100

    saved commit

--- src/components/Footer/footerEvents/footerEvents.js
+++ src/components/Footer/footerEvents/footerEvents.js
@@ -9,17 +9,30 @@ const FooterEvents = () => {
         <>
           <section className='footer-section'>
               <div className='footer-main'>
-                  <h4>Global Assembly</h4>
-                  <p>Along Aso drive Maitama Abuja</p>
+                  <h4>Global Assembly LOGO</h4>
+                  <p>Welcome to Global Assembly ministries, a Jesus-centered church in Abuja</p>
                   <p><span>Contact Us</span> +234-9058833298</p>
               </div>
               <div className='footer-sub'>
-                  <h4>Join our Mailing List</h4>
-                  <input className='input'/><Button>Sign Up</Button>
+                  <h4>UPCOMING SERVICES</h4>
+                  <p>Sunday 9 AM Early Service</p>
+                  <p>11 AM Morning Service</p>
+                  <p>Wednesday 5 PM Bible study</p>                  
+                  <p>Friday 5 PM Evening Service</p>
               </div>
               <div className='footer-links'>
                   <h4>Follow Us</h4>
+                  <p>Stay connected with Christ Unveiled across several social media 
+                      platfroms.
+                  </p>
                   <SocialM />
+              </div>
+              <div className='footer-support'>
+                <h4>DONATE & SUPPORT</h4>
+                <p>Giving our resources strengthens our relationship with God
+                  and frees us to live open-handedly, trusting God with our finances.    
+                </p>
+                <Button> DONATE TO GA</Button>
               </div>
           </section>
           
--- src/components/Social-M/Social-M.js
+++ src/components/Social-M/Social-M.js
@@ -6,9 +6,9 @@ import Line from '../Button/under-line';
 const SocialM = () => {
     return ( 
             <ul className='social-list'>
-                <a href='www.facebook.com' ><i className='pi pi-facebook' style={{'fontSize': '1.5em'}}></i></a>
-                <a href='www.youtube.com' ><i className='pi pi-youtube' style={{'fontSize': '1.5em'}}></i></a>
-                <a href='www.twitter.com' ><i className='pi pi-twitter' style={{'fontSize': '1.5em'}} ></i></a>
+                <a href='https://www.facebook.com' ><i className='pi pi-facebook' style={{'fontSize': '1.5em'}}></i></a>
+                <a href='https://www.youtube.com' ><i className='pi pi-youtube' style={{'fontSize': '1.5em'}}></i></a>
+                <a href='https://www.twitter.com' ><i className='pi pi-twitter' style={{'fontSize': '1.5em'}} ></i></a>
             </ul>
      );
 }
