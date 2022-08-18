298b45e9348bb3d5fad1fbd89141939af7aa547a
Gerald Ezenagu <egerald344@gmail.com>
2022-08-18 22:58:04 +0100

    almost done

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -14,24 +14,24 @@ const Contact = () => {
             <h1 className='contact-text'>Contact Form</h1>
 
             <div className='form main'>
-              <label className='label'>Name</label>
-              <input className='form-name' type='text' placeholder='First Name' name='fname'/>
-              <input className='form-name' type='text' placeholder='Last Name' name='lname'/>
+              <label >Name</label>
+              <input  type='text' placeholder='First Name' name='fname'/>
+              <input  type='text' placeholder='Last Name' name='lname'/>
             </div>
 
             <div className='form digits'>
-              <label className='label'>Phone</label>
-              <input className='form-phone' type='number' placeholder='#### ### ####' name='number'/>
+              <label >Phone</label>
+              <input  type='number' placeholder='#### ### ####' name='number'/>
             </div>
 
             <div className='form email'>
-              <label className='label'>Email</label>
-              <input className='form-mail' type='email' placeholder='Email' name='mail'/>
+              <label >Email</label>
+              <input type='email' placeholder='Email' name='mail'/>
             </div>
           
             <div className='form info'>
-              <label className='label'>Could we have some details about your question?</label>
-              <textarea className='form-details' type='text' name='details'/>
+              <label >Question?</label>
+              <textarea type='text' name='details'/>
             </div>
             <Button change='btn1'>SEND</Button>
           </form>
